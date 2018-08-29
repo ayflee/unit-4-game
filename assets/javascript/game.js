d6b0ec3db2a1bb9d677fffa57772d9773078df3b
ALP <annielee@Annies-MacBook-Pro.local>
2018-08-29 15:01:43 -0700

    code

---
+++ assets/javascript/game.js
@@ -0,0 +1,108 @@
+$(document).ready(function () {
+
+    //randomNumber is the number shown at the start of the game//
+    var randomNumber;
+
+    //randomNumber is between 19-120//
+    randomNumber = Math.floor(Math.random () * 101 +19);
+    $("#computerNumber").text(randomNumber);
+
+    //set variables for the value of each gem, which is between 1-12//
+    var gem1= Math.floor(Math.random () * 11 + 1);
+    var gem2= Math.floor(Math.random () * 11 + 1);
+    var gem3= Math.floor(Math.random () * 11 + 1);
+    var gem4= Math.floor(Math.random () * 11 + 1);
+        
+    //set variables for wins and losses//
+    var wins=0;
+    var losses=0;
+    var totalScore=0;
+
+    $("#numberWins").text(wins);
+    $("#numberLosses").text(losses);
+
+    //resetting the game, for random number and for the gem values//
+    function reset() {
+        randomNumber = Math.floor(Math.random () * 101 +19);
+        $("#computerNumber").text(randomNumber);
+
+        var gem1= Math.floor(Math.random () * 11 + 1);
+        var gem2= Math.floor(Math.random () * 11 + 1);
+        var gem3= Math.floor(Math.random () * 11 + 1);
+        var gem4= Math.floor(Math.random () * 11 + 1);
+
+
+        totalScore=0;
+        $("#userTotal").text(totalScore);
+    }
+
+    //Adding wins to the total//
+    function totalWins() {
+        alert("You Won!!!");
+        wins++;
+        // $("#numberWins").text(wins);
+        reset();
+    }
+
+    //Adding losses to the total//
+    function totalLosses() {
+        alert("You lost...Boohoo");
+        losses++;
+        // $("numberLosses").text(losses);
+        reset();
+    }
+
+    //Listening to gem clicks and tallying total score//
+
+    $("#gem1").on("click", function () {
+        totalScore = totalScore + gem1;
+        $("#userTotal").text (totalScore);
+            if (totalScore == randomNumber) {
+                totalWins();
+            }
+            else if (totalScore > randomNumber) {
+                totalLosses();
+            }
+    });
+
+    $("#gem2").on("click", function () {
+        totalScore = totalScore + gem2;
+        $("#userTotal").text (totalScore);
+            if (totalScore == randomNumber) {
+                totalWins();
+            }
+            else if (totalScore > randomNumber) {
+                totalLosses();
+            }
+    });
+
+    $("#gem3").on("click", function () {
+        totalScore = totalScore + gem3;
+        $("#userTotal").text (totalScore);
+            if (totalScore == randomNumber) {
+                totalWins();
+            }
+            else if (totalScore > randomNumber) {
+                totalLosses();
+            }
+    
+    });
+
+    $("#gem4").on("click", function () {
+        totalScore = totalScore + gem4;
+        $("#userTotal").text (totalScore);
+            if (totalScore == randomNumber) {
+                totalWins();
+            }
+            else if (totalScore > randomNumber) {
+                totalLosses();
+            }
+
+    });
+
+    var html = 
+        "<p>Wins: " + wins + "</P>" +
+        "<p>Losses: " + losses + "</p";
+    document.getElementByID("game").innerhtml = html;
+
+});
